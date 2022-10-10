22867b6c8eb0df7db40908b43bcee966b4d3cf41
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-10-10 13:14:15 +0530

    Type casting and DateTime STruct

---
+++ 10-10-22 datatype in c sharp/DateTimeValuetype.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bands
+{
+    public class DateTimeValuetype
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Today);
+
+        }
+
+
+
+
+
+    }
+}
---
+++ 10-10-22 datatype in c sharp/Typechanging.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bands
+{
+    public class Typechanging
+    {
+        public static void Main(String[] args)
+        {
+            //implicit by compiler (smaller to bigger)
+            int a = 1234567;
+            long l = a;
+
+            //Explicit Casting (bigger to smaller)
+            long l1 = 12345;
+            int a1 = (int)l1;
+
+            //Using Convert.Changetype helper bigger to smaller
+
+            double d = 1267.77;
+            int a2= (int) Convert.ChangeType(d, typeof(int));
+
+        }
+    }
+}
