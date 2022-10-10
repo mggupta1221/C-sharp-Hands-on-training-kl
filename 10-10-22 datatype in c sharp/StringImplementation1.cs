552f81e544891152996be75cbc99ee640d997dae
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-10-10 16:36:04 +0530

    String methods

---
+++ 10-10-22 datatype in c sharp/StringImplementation1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bands
+{
+    public class StringImplementation1
+    {
+
+        //public static void Main(String[] args)
+        //{
+        //    public string s = " asefdf jka kajskoe ekj ejhd iia ";
+        //    string firstName = "Mukesh";
+        //    string lastName = "Gupta";
+
+        //    //concatenation
+        //    string fullName = firstName + lastName;
+        //string Upper=fullName.ToUpper();
+        //string lowerr=fullName.ToLower();
+
+
+        //}
+
+    }
+}
