f59bcf86b96a78fc70124daaafb8345fdf892a2b
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-10-10 16:48:29 +0530

    String methods

---
+++ 10-10-22 datatype in c sharp/Stringreversal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bands
+{
+     class Stringreversal
+    {
+        public static void main()
+        {
+            string s = Console.ReadLine();
+
+            string reverse = "";
+            for (int i = (s.Length - 1); i >= 0; i--)
+            {
+
+                reverse = reverse + s[i];
+
+            }
+            Console.WriteLine(reverse);
+        }
+    }
+}
