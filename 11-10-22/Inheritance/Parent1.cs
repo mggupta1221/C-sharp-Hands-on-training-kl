c0c28d377d9084d409f2c3775e0b138adc30833e
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-10-11 13:02:28 +0530

    inhetitance Example

---
+++ 11-10-22/Inheritance/Parent1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Parent1
+    {
+        //Parent1()
+        //{
+        //    Console.WriteLine("Private Parent1 COnstructor called");
+        //}
+
+        //parent class cannot access child class constructor
+
+        public Parent1()
+        {
+            Console.WriteLine("public parent1 constructor called");
+        }
+
+        public Parent1(int number)
+        {
+            Console.WriteLine("public parent1 parameterized constructor called: "+number);
+        }
+
+        public void test1()
+        {
+            Console.WriteLine("Test 1 method");
+
+        }
+
+        public void test2()
+        {
+            Console.WriteLine("Test 2 method");
+
+        }
+    }
+}
---
+++ 11-10-22/Inheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+//The default parent class of any class is Object 
+
+namespace Inheritance
+{
+
+    public class Program:Parent1
+    { 
+            Program(int a):base(a)
+            {
+                Console.WriteLine("Child Constructor called");
+            }
+            static void Main(string[] args)
+            {
+                Program c = new Program(55); //implicitely go to parent constuctor calls it then calles own constructor
+                c.test1();
+                c.test2();   
+            }
+
+       
+    }
+}
