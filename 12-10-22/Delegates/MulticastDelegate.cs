565fdbbc0df3440903f4a573685b259aa7deceb4
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-10-12 17:58:33 +0530

    Multicast delegates with reurnint type

---
+++ 12-10-22/Delegates/MulticastDelegate.cs
@@ -0,0 +1,37 @@
+﻿
+
+public class Rectangle
+{
+
+    //the method defination of perimeter and area is same
+    public void getArea(double length, double breadth)
+    {
+        Console.WriteLine("Area Of rectangle:" + (length * breadth));
+    }
+
+    public void getPerimeter(double length, double breadth)
+    {
+        Console.WriteLine("perimeter Of rectangle:" + (2 * (length + breadth)));
+    }
+
+    public delegate void rectangleDelegate(double lengthOfRectangle, double breadthOfrectangle);
+
+
+
+    public static void Main()
+    {
+        Rectangle rect = new Rectangle();
+
+
+        rectangleDelegate rectangleDelegateObject = new rectangleDelegate(rect.getPerimeter);
+        //or
+        //rectangleDelegate rectangleDelegateobject = rect.getArea;//wihhout calling constructor also we can define
+
+        rectangleDelegateObject += rect.getArea;
+
+        rectangleDelegateObject.Invoke(32.5, 33); //both methods get called 1.getArea 2.getPerimeter whichever added first gets called first
+        Console.WriteLine();
+        rectangleDelegateObject.Invoke(100, 56.8);
+
+    }
+}
---
+++ 12-10-22/Delegates/MulticastDelegateWithReturningValues.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class Rectangle
+    {
+
+        //the method defination of perimeter and area is same
+        public double getArea(double length, double breadth)
+        {
+            return (length * breadth);
+        }
+
+        public double getPerimeter(double length, double breadth)
+        {
+            return (2 * (length + breadth));
+        }
+
+        public delegate double rectangleDelegate(double lengthOfRectangle, double breadthOfrectangle);
+
+
+
+        public static void Main()
+        {
+            Rectangle rect = new Rectangle();
+
+
+            rectangleDelegate rectangleDelegateObject = new rectangleDelegate(rect.getArea);
+            //or
+            //rectangleDelegate rectangleDelegateobject = rect.getArea;//wihhout calling constructor also we can define
+
+            rectangleDelegateObject += rect.getPerimeter; //result of first overriden by second so in multicast delegates it is good to use void type of methods
+
+
+            double x = rectangleDelegateObject(33, 1);
+            Console.WriteLine(x);
+
+        }
+    }
+}
+
