af561bda67311896572d8ea6d7789eb63b5bd234
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-10-13 10:01:15 +0530

    Calculator using Delegates

---
+++ 13-10-22/Calculator using Delegates/Calculate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_using_Delegates
+{
+    internal class Calculate
+    {
+        public delegate void CalculateDelegate(double x, double y);
+
+
+        public void CalAdd(double x, double y)
+        {
+            Console.WriteLine("Addition :" + (x + y));
+        }
+
+        public void CalSubtract(double x, double y)
+        {
+            Console.WriteLine("Subtraction :" + (x - y));
+        }
+
+        public void CalMultiply(double x, double y)
+        {
+            Console.WriteLine("Multiplication :" + (x * y));
+        }
+
+        public void CalDivide(double x, double y)
+        {
+            Console.WriteLine("Division :" + (x / y));
+        }
+
+        public void CalRemainderOrModulus(double x, double y)
+        {
+            Console.WriteLine("Modulus :" + (x % y));
+        }
+    }
+}
---
+++ 13-10-22/Calculator using Delegates/Program.cs
@@ -0,0 +1,13 @@
+﻿using Calculator_using_Delegates;
+using static Calculator_using_Delegates.Calculate;
+
+Calculate cal = new Calculate();
+CalculateDelegate calDelegateObject = new CalculateDelegate(cal.CalAdd);
+
+calDelegateObject += cal.CalSubtract;
+    calDelegateObject += cal.CalMultiply;
+calDelegateObject += cal.CalDivide;
+
+calDelegateObject += cal.CalRemainderOrModulus;
+
+calDelegateObject(2.3, 4.5);
