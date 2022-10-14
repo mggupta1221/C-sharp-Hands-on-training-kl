a118385de2a6d0e5e678e296388a635412eaa758
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-10-14 15:53:30 +0530

    Sealed class/method implementation

---
+++ 14-10-22/Sealed Class/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+//sealed classes used if u dont want anyone to derive from your class
+//eg ui component 
+
+namespace Sealed_Class
+{
+
+    sealed class SealedClass
+    {
+        
+    }
+
+    //public class SimpleClass : SealedClass { } //Error cannot derive from sealed class
+
+    public class SimpleClass
+    {
+        public void Test1()
+        {
+            Console.WriteLine("simple class test1");
+        }
+
+        public virtual void Test2()
+        {
+            Console.WriteLine("simple class virtual test2");
+        }
+
+    }
+
+    public class DerivedSimpleClass : SimpleClass
+    {
+        public override sealed void Test2() // u can override and make base method sealed
+        {
+            //sealed method cannot be overridden
+        }
+
+
+    }
+}
+    
+    
