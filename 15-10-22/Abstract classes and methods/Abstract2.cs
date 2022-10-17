7a18d1d4195289a3008792016a937efc3cb938a0
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-10-17 10:23:57 +0530

    Abstract class and methods

---
+++ 15-10-22/Abstract classes and methods/Abstract2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_classes_and_methods
+{
+    public abstract class Abstract2
+    {
+
+        public void Add(int x,int y)
+        {
+            Console.WriteLine("Addition: " + (x + y));
+        }
+        public void Sub(int x, int y)
+        {
+            Console.WriteLine("Subtraction: " + (x - y));
+        }
+        public abstract void Mul(int x, int y);
+        public abstract void div(int x, int y);
+
+    }
+
+    public class DerivedAbstract2:Abstract2
+    {
+        public static void Main()
+        {
+            DerivedAbstract2 d=new DerivedAbstract2();
+
+            Abstract2 a = d; //abstract method instance cannot be created but reference can be creacted that reference can call overridden methods of abstract parent class
+
+            d.Mul(1,2);
+            d.div(1,2);
+            d.Add(1,2);
+            d.Sub(1,2);
+            Console.WriteLine("Using Parent abstract reference:");
+            a.Mul(1, 2);
+            a.div(1, 2);
+            a.Add(1, 2);
+            a.Sub(1, 2);
+
+        }
+
+        public override void div(int x, int y)
+        {
+            Console.WriteLine("Multiplication: " + (x * y));
+        }
+
+        public override void Mul(int x, int y)
+        {
+            Console.WriteLine("Division: " + (x / y));
+        }
+    }
+
+}
---
+++ 15-10-22/Abstract classes and methods/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace Abstract_clases_and_methods
+{
+    public abstract class Figure  // abstract methods can contain abstract and non abstract methosd it is must for child to override all abstract methods fron parent abstracyt class
+    {
+        public double length, breadth, radius,height;
+        public const double pi = 3.142;
+
+        public abstract double GetArea(); //method name/signature will be same in all derived //it must be overridden in child class
+        public void names()
+        {
+
+        }
+    }
+    //child can consume non abstract methods only if it has implemented all the abstract methods in parent abstract class
+    public class Rectangle : Figure
+    {
+
+        public Rectangle(double length,double breadth)
+        {
+            this.length = length;       
+            this.breadth = breadth;
+        }
+        public override double GetArea()
+        {
+            return length * breadth;
+        }
+    }
+
+
+    public class Circle : Figure
+    {
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+          
+        }
+        public override double GetArea()
+        {
+            return pi * radius * radius; ;
+        }
+    }
+
+
+    public class Cylinder : Figure
+    {
+
+        public Cylinder(double radius,double height)
+        {
+            this.height = height;
+            this.radius = radius;
+        }
+        public override double GetArea()
+        {
+            return 2*pi*((radius*height)+(radius*radius));
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Rectangle rectangleObj = new Rectangle(2.3, 4.5);
+            Circle circleObj = new Circle(8.9);
+            Cylinder cylinderObj = new Cylinder(4.5, 5.6);
+
+            double areaOfRectangle = rectangleObj.GetArea();
+            double areaOfCircle = circleObj.GetArea();
+            double areaOfCylinder = cylinderObj.GetArea();
+
+            Console.WriteLine($" Area Of Rectangle:{areaOfRectangle}\n Area OfCircle:{areaOfCircle}\n Area of Cylinder:{areaOfCylinder} ");
+
+        }
+    }
+}
