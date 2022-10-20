e0187f824e07f23f6fca87ceeaf612c51ceba26c
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-10-20 17:30:33 +0530

    Indexer: to access class numbers by indexing(int ,string,char)

---
+++ 20-10-22/Indexers/Indexer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+//indexers used to access class members through indexing
+
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexersDemo
+{
+    public class Indexer
+    {
+        public int Id { get;}
+        public string Name { get;}
+
+        public string Department { get;}
+        public int Salary { get; }
+
+        public Indexer(int id,string name,string department,int salary)
+        {
+                this.Id = id;   
+             this.Name = name;
+            this.Department = department;
+            this.Salary = salary;
+        }
+
+
+        public object this[int index]
+        {
+            get
+            {
+                if (index == 1)
+                {
+                    return Id;
+                }
+                else if (index == 2)
+                {
+                    return Name;
+                }
+                else if (index == 3)
+                {
+                    return Department;
+                }
+                else if (index == 4)
+                {
+                    return Salary;
+                }
+                return null;
+
+            }
+
+        }
+
+
+    }
+}
---
+++ 20-10-22/Indexers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IndexersDemo
+{
+    public class Program
+    {
+
+        public static void Main()
+        {
+            Indexer indexerObject1 = new Indexer(100,"Albert","physics",25000);
+
+            for(int count = 1; count <= 4; count++)
+            {
+                Console.WriteLine(indexerObject1[count]);
+            }
+
+        }
+    }
+
+
+
+
+}
