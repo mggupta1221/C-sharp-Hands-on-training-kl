52e27161f0af60b8002e4b4268db7b17d94b2ed1
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-10-21 10:54:09 +0530

    List Implementation

---
+++ 21-10-22/List/BusRouteRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace BusRouteApplication
+{
+    public class BusRouteRepository
+    {
+
+
+        public static List<BusRoute> InitializeRoutes()
+        {
+            List<BusRoute> result = new List<BusRoute>();//list is empty after executing the constructor
+          
+                result.Add(new BusRoute(40,new string []{"Morcambe","Lancaster","Garstang","Preston"}));
+                result.Add(new BusRoute(42,new string []{ "Lancaster", "Garstang","Blackpool"}));
+                result.Add(new BusRoute(1_000,new string []{ "TestOrigin","TestDestination"}));
+                result.Add(new BusRoute(100,new string []{"University", "Lancaster", "Morecambe" }));
+                result.Add(new BusRoute(555,new string []{ "Lancaster","Carnforth","Kendel","Windermere","Keswik"}));
+            return result;
+        }
+
+    }
+}
---
+++ 21-10-22/List/Program2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;//collections are provided
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//need of List<> :array cannot remove the specific element at specific index
+namespace BusRouteApplication
+{
+    public class Program2
+    {
+        public static void Main()
+        {
+
+            List<BusRoute> allRoutes = BusRouteRepository.InitializeRoutes();
+            
+            Console.WriteLine($"Before Removing:Elements={allRoutes.Count}");
+             foreach(BusRoute route in allRoutes)
+                    Console.WriteLine($"Route: {route}");
+             Console.WriteLine();
+
+
+            
+            //allRoutes.Remove(allRoutes[2]);
+            //allRoutes.RemoveAt(2) ;
+            allRoutes.RemoveAll(route =>route.Origin.StartsWith("Test"));
+             Console.WriteLine($"After Removing:Elements={allRoutes.Count}");
+             foreach(BusRoute route in allRoutes)
+                    Console.WriteLine($"Route: {route}");
+             Console.WriteLine();
+        }  
+        
+    }
+}
